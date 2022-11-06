985f791725d69a09650adace62221a47aedb7a58
Styazhko <styazhko97@gmail.com>
2022-11-06 19:09:10 +0400

    Add new method

--- yandex_disk.py
+++ yandex_disk.py
@@ -6,6 +6,7 @@ class YandexDisk:
     def __init__(self, API_TOKEN):
         self.token = API_TOKEN
 
+    '''Создание папки'''
     def create_folder(self, name):
         URL = "https://cloud-api.yandex.net/v1/disk/resources"
         headers = {
@@ -15,4 +16,16 @@ class YandexDisk:
             }
         params = {"path": f"{name}"}
         response = requests.put(URL, headers=headers, params=params)
+        return response
+
+    '''Удаление папки'''
+    def delete_folder(self, name):
+        URL = "https://cloud-api.yandex.net/v1/disk/resources"
+        headers = {
+            "Authorization": f"OAuth {self.token}", 
+            "Content-Type": "application/json", 
+            "Accept": "application/json",
+            }
+        params = {"path": f"{name}"}
+        response = requests.delete(URL, headers=headers, params=params)
         return response
