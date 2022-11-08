6082c0edb94e68dbe09c508025510abd2ade910a
Styazhko <styazhko97@gmail.com>
2022-11-08 18:54:44 +0400

    delete token

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 '''
-Токен был получен при помощи client_id используя url - https://oauth.yandex.ru/authorize?response_type=token&client_id=<идентификатор приложения>
+Токен необходимо получить при помощи client_id используя url - https://oauth.yandex.ru/authorize?response_type=token&client_id=<идентификатор приложения>
 '''
-API_TOKEN = "y0_AgAAAABly9BEAAiNzgAAAADTFq5tO2KMMcNFROybDSzjJPRCOh2HphA"
+API_TOKEN = ""
